70d9c2fc792bb3cc3748ad640fd09d63b84f27f1
fullmeo <serignetrumpet@gmail.com>
2025-06-10 20:56:08 +0200

    fix: definitively remove LanguageSwitcher dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 // app/layout.tsx
 import './globals.css';
 import type { ReactNode } from 'react';
-import LanguageSwitcher from '@/components/LanguageSwitcher';
 
 export default function RootLayout({ children }: { children: ReactNode }): JSX.Element {
   return (
     <html lang="fr">
       <body>
-        <LanguageSwitcher />
         {children}
       </body>
     </html>
